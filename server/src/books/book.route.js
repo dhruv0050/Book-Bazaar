b6dd6aafe643b5b021629b3460c364937ab45efd
Dhruv Sharma <dhruvsh5467@gmail.com>
2024-12-06 16:03:40 +0530

    In backend Created admin and token verification

--- server/src/books/book.route.js
+++ server/src/books/book.route.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, updateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router = express.Router();
 
-router.post("/create-book",postABook)
+router.post("/create-book",verifyAdminToken,postABook)
 
 router.get("/",getAllBooks)
 
 router.get("/:id",getSingleBook)
 
-router.put("/edit/:id", updateBook)
+router.put("/edit/:id",verifyAdminToken ,updateBook)
 
-router.delete("/:id", deleteABook)
+router.delete("/:id", verifyAdminToken,deleteABook)
 
 module.exports = router;
