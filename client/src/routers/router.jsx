bf50cbff67f8e41a9fad7977456367b0dedbd0a5
Dhruv Sharma <dhruvsh5467@gmail.com>
2024-12-07 12:13:33 +0530

    Admin router and dashboard login

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -10,6 +10,8 @@ import CheckoutPage from "../pages/books/CheckoutPage";
 import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
 import OrderPage from "../pages/books/OrderPage";
+import AdminRoute from "./AdminRoute";
+import AdminLogin from "../components/AdminLogin";
 const router = createBrowserRouter([
     {
         path: "/",
@@ -53,6 +55,32 @@ const router = createBrowserRouter([
             },
         ]
     },
+    {
+        path: "/admin",
+        element: <AdminLogin/>
+    },
+    {
+        path: "/dashboard",
+        element: <AdminRoute><div>Admin Dashboard</div></AdminRoute>,
+        children: [
+            {
+                path: "",
+                element: <AdminRoute><div>Admin Dashboard</div></AdminRoute>
+            },
+            {
+                path: "add-new-book",
+                element: <AdminRoute><div>Add new book</div></AdminRoute>
+            },
+            {
+                path: "edit-book/:id",
+                element: <AdminRoute><div>Edit books</div></AdminRoute>
+            },
+            {
+                path: "manage-books",
+                element: <AdminRoute><div>Manage books</div></AdminRoute>
+            },
+        ]
+    }
 ]);
 
 export default router;
